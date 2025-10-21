31639a1ad1afedcbd0117daf51be13e670107546
Nischhelm <nischhelm@gmail.com>
2025-10-21 23:35:00 +0200

    v1.0.5 this was painful
    - auto remap enchantment ids in worlds/servers with different ids

--- src/main/java/citpatch/OptiFineCITPatch.java
+++ src/main/java/citpatch/OptiFineCITPatch.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class OptiFineCITPatch {
     public static final String MODID = "optifinecitpatch";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
     public static final String NAME = "OptiFine CIT Patch";
     public static final Logger LOGGER = LogManager.getLogger();
 
